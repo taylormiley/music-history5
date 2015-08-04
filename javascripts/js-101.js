709812da8f60607fa552f0c259d6b3dbc7cd6bc4
Taylor Miley <ChiefDoogin@Taylors-MacBook-Pro.local>
2015-08-04 11:38:28 -0500

    added bootstrap panels and some minor styling changes

--- javascripts/js-101.js
+++ javascripts/js-101.js
@@ -4,6 +4,7 @@ requirejs(
     var outputList = document.getElementById("container"); 
     var outputDiv;
     var removeButton;
+    var expandButton;
 
     pop.querySongs(function(data2) {
       console.log("data", data2);
@@ -11,30 +12,43 @@ requirejs(
       for(var i = 0; i < data2.length; i++) {
   
         outputDiv = document.createElement("div");
+        outputDiv.className = "outputDiv panel panel-default";
         outputList.appendChild(outputDiv);
+
+        var panelHeading = document.createElement("div");
+        panelHeading.className = "panel-heading";
+        outputDiv.appendChild(panelHeading);
   
         var h3 = document.createElement("h3");
+        h3.className = "panel-title";
         h3.innerHTML = data2[i].name;
-        outputDiv.appendChild(h3);
+        panelHeading.appendChild(h3);
+
+        var panelBody = document.createElement("div");
+        panelBody.className = "panel-body";
+        outputDiv.appendChild(panelBody);
+
         
         var span = document.createElement("span");
         span.innerHTML = data2[i].artist;
-        outputDiv.appendChild(span);
+        panelBody.appendChild(span);
        
         var span1 = document.createElement("span");
         span1.innerHTML = data2[i].album;
-        outputDiv.appendChild(span1);
+        panelBody.appendChild(span1);
         var removeButton = document.createElement("button");
         removeButton.id = "removeButton";
         $(removeButton).attr("type", "button");
         $(removeButton).text("Remove");
-        outputDiv.appendChild(removeButton);
+        panelBody.appendChild(removeButton);
+
+        var expandButton = document.getElementById("expandButton");
+        outputList.appendChild(expandButton);
+        $("#expandButton").attr("type", "button");
       }
     });
 
-    var expandButton = document.getElementById("expandButton");
-    outputList.appendChild(expandButton);
-    $("#expandButton").attr("type", "button");
+    
     
     $("#expandButton").on("click", function() {
       get_more.querySongs(function(data2) {
@@ -43,30 +57,40 @@ requirejs(
         for(var i = 0; i < data2.length; i++) {
   
           outputDiv = document.createElement("div");
-          outputList.appendChild(outputDiv);
+          outputDiv.className = "outputDiv panel panel-default";
+          $("#expandButton").before(outputDiv);
+
+          var panelHeading = document.createElement("div");
+          panelHeading.className = "panel-heading";
+          outputDiv.appendChild(panelHeading);
   
           var h3 = document.createElement("h3");
+          h3.className = "panel-title";
           h3.innerHTML = data2[i].name;
-          outputDiv.appendChild(h3);
+          panelHeading.appendChild(h3);
+
+          var panelBody = document.createElement("div");
+          panelBody.className = "panel-body";
+          outputDiv.appendChild(panelBody);
         
           var span = document.createElement("span");
           span.innerHTML = data2[i].artist;
-          outputDiv.appendChild(span);
+          panelBody.appendChild(span);
        
           var span1 = document.createElement("span");
           span1.innerHTML = data2[i].album;
-          outputDiv.appendChild(span1);
+          panelBody.appendChild(span1);
           var removeButton = document.createElement("button");
           removeButton.id = "removeButton";
           $(removeButton).attr("type", "button");
           $(removeButton).text("Remove");
-          outputDiv.appendChild(removeButton);
+          panelBody.appendChild(removeButton);
           
         }
       });
     });
     $(document).on("click", "#removeButton", function () {
-      $(this).parent().remove();
+      $(this).parent().parent().remove();
     });
 
   }
